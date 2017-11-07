7cacd6622bf690fecd00b2a146876c40ae7f8bc7
xiaozhong <wwwbjqcom@126.com>
2017-11-07 14:24:06 +0800

    D

--- byte_read.h
+++ byte_read.h
@@ -1,32 +1,38 @@
+#ifndef _BYTE_READ_H_
+
 #include "fsp.h"
 
 /********************************************************//**
 														  The following function is used to fetch data from 4 consecutive
 														  bytes. The most significant byte is at the lowest address.
 														  @return ulint integer */
-extern ulint mach_read_from_4();
+extern ulint mach_read_from_4;
 
 
 /********************************************************//**
 														  The following function is used to fetch data from 3 consecutive
 														  bytes. The most significant byte is at the lowest address.
 														  @return ulint integer */
-extern ulint mach_read_from_3();	/*!< in: pointer to 3 bytes */
+extern ulint mach_read_from_3;	/*!< in: pointer to 3 bytes */
 
 
 
-/********************************************************//**
-														  The following function is used to fetch data from 2 consecutive
-														  bytes. The most significant byte is at the lowest address.
-														  @return ulint integer */
-extern ulint
-mach_read_from_2();	/*!< in: pointer to 2 bytes */
+								/********************************************************//**
+																						  The following function is used to fetch data from 2 consecutive
+																						  bytes. The most significant byte is at the lowest address.
+																						  @return ulint integer */
+extern ulint mach_read_from_2;	/*!< in: pointer to 2 bytes */
 
 
-/********************************************************//**
-														  The following function is used to fetch data from one byte.
-														  @return ulint integer, >= 0, < 256 */
-extern ulint mach_read_from_1();	/*!< in: pointer to byte */
+					/********************************************************//**
+																			  The following function is used to fetch data from one byte.
+																			  @return ulint integer, >= 0, < 256 */
+extern ulint mach_read_from_1;	/*!< in: pointer to byte */
+
+#endif // !_BYTE_READ_H_
+
+
+
 
 
 
