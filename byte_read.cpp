d7e5ce971c604574680c6ed31483e08e6f9886f3
xiaozhong <wwwbjqcom@126.com>
2017-11-07 14:17:05 +0800

    1

--- byte_read.cpp
+++ byte_read.cpp
@@ -4,34 +4,52 @@
 														  The following function is used to fetch data from 4 consecutive
 														  bytes. The most significant byte is at the lowest address.
 														  @return ulint integer */
-extern ulint mach_read_from_4();
+ulint mach_read_from_4(const char* b)	/*!< in: pointer to four bytes */
+{
+	return(((ulint)(b[0]) << 24)
+		| ((ulint)(b[1]) << 16)
+		| ((ulint)(b[2]) << 8)
+		| (ulint)(b[3])
+		);
+}
 
 
 /********************************************************//**
 														  The following function is used to fetch data from 3 consecutive
 														  bytes. The most significant byte is at the lowest address.
 														  @return ulint integer */
-extern ulint mach_read_from_3();	/*!< in: pointer to 3 bytes */
+ulint mach_read_from_3(
+	/*=============*/
+	const char*	b)	/*!< in: pointer to 3 bytes */
+{
 
+	return(((ulint)(b[0]) << 16)
+		| ((ulint)(b[1]) << 8)
+		| (ulint)(b[2])
+		);
+}
 
 
 /********************************************************//**
 														  The following function is used to fetch data from 2 consecutive
 														  bytes. The most significant byte is at the lowest address.
 														  @return ulint integer */
-extern ulint
-mach_read_from_2();	/*!< in: pointer to 2 bytes */
-
+ulint
+mach_read_from_2(
+	/*=============*/
+	const char*	b)	/*!< in: pointer to 2 bytes */
+{
+	return(((ulint)(b[0]) << 8) | (ulint)(b[1]));
+}
 
 /********************************************************//**
 														  The following function is used to fetch data from one byte.
 														  @return ulint integer, >= 0, < 256 */
-extern ulint mach_read_from_1();	/*!< in: pointer to byte */
-
-
-
-
-
-
-
+ulint
+mach_read_from_1(
+	/*=============*/
+	const char*	b)	/*!< in: pointer to byte */
+{
+	return((ulint)(b[0]));
+}
 
