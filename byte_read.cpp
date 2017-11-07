129e0ce61116a2e631742655194208ad0032b744
xiaozhong <wwwbjqcom@126.com>
2017-11-07 17:30:47 +0800

    d

--- byte_read.cpp
+++ byte_read.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 
 //convert the byte of Big-Endian and Little-Endian
-int conversion_byte_order(int num)
+int conversion_byte_order_4(int num)
 {
 	uint tmp;
 	tmp = ((num & 0x000000FF) << 24) |
@@ -15,6 +15,22 @@ int conversion_byte_order(int num)
 	return tmp;
 }
 
+int conversion_byte_order_3(short num)
+{
+	uint tmp;
+	tmp = ((num & 0x0000FF) << 16) |
+		((num & 0x00FF00)) |
+		((num & 0xFF0000) >> 16);
+	return tmp;
+}
+
+int conversion_byte_order_2(short num)
+{
+	uint tmp;
+	tmp = ((num & 0x00FF) << 8) |
+		((num & 0xFF00) >> 8);
+	return tmp;
+}
 
 /********************************************************//**
 														  The following function is used to fetch data from 4 consecutive
@@ -24,7 +40,7 @@ ulint mach_read_from_4(const char* b)	/*!< in: pointer to four bytes */
 {
 
 
-	return conversion_byte_order((((ulint)(b[0]) << 24)
+	return conversion_byte_order_4((((ulint)(b[0]) << 24)
 		| ((ulint)(b[1]) << 16)
 		| ((ulint)(b[2]) << 8)
 		| (ulint)(b[3])
@@ -51,7 +67,7 @@ ulint mach_read_from_3(
 	const char*	b)	/*!< in: pointer to 3 bytes */
 {
 
-	return(((ulint)(b[0]) << 16)
+	return conversion_byte_order_3(((ulint)(b[0]) << 16)
 		| ((ulint)(b[1]) << 8)
 		| (ulint)(b[2])
 		);
@@ -78,7 +94,7 @@ mach_read_from_2(
 	/*=============*/
 	const char*	b)	/*!< in: pointer to 2 bytes */
 {
-	return(((ulint)(b[0]) << 8) | (ulint)(b[1]));
+	return conversion_byte_order_2(((ulint)(b[0]) << 8) | (ulint)(b[1]));
 }
 
 ulint
